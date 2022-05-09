eb07129caadf56e158722b9326b5a4df67893f03
maktolun <36237922+maktolun@users.noreply.github.com>
2022-05-08 22:23:43 -0400

    Delete data directory

--- data/tetraf6_MD/run_job.sh
+++
@@ -1,5 +0,0 @@
-#This script will run the MD simulation of the given molecule (.xyz file)
-#with the provided paramaters in the .inp and .job files by allocating requested
-#resources on XSEDE. Output file is the MD trajectory with the extension of .trj
-
-sbatch tetraf6.job
