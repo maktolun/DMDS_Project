f0e4c1ee3ce4ffed4cb79372380085a3649c7832
maktolun <36237922+maktolun@users.noreply.github.com>
2022-05-08 22:12:19 -0400

    Update predi-xy.py

--- predi-xy.py
+++ predi-xy.py
@@ -1,8 +1,12 @@
+#Mounting Google Drive to access files in the folder
 from google.colab import drive
 drive.mount('/content/drive')
 
+#Changing directory to working directory
 %cd /content/drive/MyDrive/Colab_Notebooks/project
 
+#Installing Predi-XY by pip
 !pip install predixy
 
+#Running Predi-XY as a terminal command
 !predixy /content/drive/MyDrive/Colab_Notebooks/project/furandimer.xyz
